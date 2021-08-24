d53d4c904df8f39c87297ce241aa24c768eb90dc
Kekin Alexey <agkekin@gmail.com>
2021-08-24 23:53:16 +0300

    Task5 solved

--- Tasks/Task5.py
+++ Tasks/Task5.py
@@ -14,12 +14,32 @@ pig_it('Hello world !')     # elloHay orldway !
 
 
 def pig_it(text):
-    pass
+    text_new = text[::]
+    text_new = text_new.rsplit()
+    list_letter = []
+    new_word = []
+    my_str = ""
+
+    for word in text_new:
+        list_letter.clear()
+
+        for letter in word:
+            list_letter.append(letter)
+
+        e1 = list_letter[0]
+        list_letter.pop(0)
+        list_letter.append(e1)
+        if letter.isalpha():
+            list_letter.append("ay")
+
+        my_str = ''.join(list_letter)
+        new_word.append(my_str)
+
+        my_str = ' '.join(new_word)
+    return my_str
+
 
 if __name__ == '__main__':
-    assert pig_it ( "Pig latin is cool" ) == "igPay atinlay siay oolcay"
-    assert pig_it ( "This is my string" ) == "hisTay siay ymay tringsay"
-    assert pig_it ( "Hello world !" ) == "elloHay orldway !"
-
-
-
+    assert pig_it("Pig latin is cool") == "igPay atinlay siay oolcay"
+    assert pig_it("This is my string") == "hisTay siay ymay tringsay"
+    assert pig_it("Hello world !") == "elloHay orldway !"
