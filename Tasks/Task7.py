27f9b90595de186d6edf5a12fa3f1026fe432f90
Kekin Alexey <agkekin@gmail.com>
2021-08-27 22:31:01 +0300

    Task7

---
+++ Tasks/Task7.py
@@ -0,0 +1,39 @@
+"""
+Is a number prime?
+
+Define a function that takes one integer argument and returns logical value true or false depending on if the integer is a prime.
+
+Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 that has no positive divisors other
+than 1 and itself.
+
+Requirements:
+
+You can assume you will be given an integer input.
+You can not assume that the integer will be only positive. You may be given negative numbers as well (or 0).
+NOTE on performance: There are no fancy optimizations required, but still the most trivial solutions might time out.
+Numbers go up to 2^31 (or similar, depends on language version). Looping all the way up to n, or n/2, will be too slow.
+
+Example
+
+is_prime(1)  /* false */
+is_prime(2)  /* true  */
+is_prime(-1) /* false */
+
+
+Определите функцию, которая принимает один целочисленный аргумент и возвращает логическое значение true или false
+в зависимости от того, является ли целое число простым.
+
+Согласно Википедии, простое число (или простое число) - это натуральное число больше 1, которое не имеет
+положительных делителей, кроме 1 и самого себя.
+
+Требования:
+
+Вы можете предположить, что вам будет предоставлен целочисленный ввод.
+Нельзя предполагать, что целое число будет только положительным. Вам также могут быть даны отрицательные числа (или 0).
+ПРИМЕЧАНИЕ по производительности: никаких изысканных оптимизаций не требуется, но все же самые тривиальные
+решения могут выйти из строя. Числа могут доходить до 2 ^ 31 (или аналогично, в зависимости от языковой версии).
+Зацикливание до n или n / 2 будет слишком медленным.
+"""
+
+def is_prime(num):
+  pass #TODO
