a96bd3171800ad8c18857af6a2a6f31666692fd4
Kekin Alexey <agkekin@gmail.com>
2022-05-20 20:23:29 +0300

    add task 11

--- Tasks/task11.py
+++ Tasks/task11.py
@@ -1,10 +1,16 @@
 def generate_hashtag(s):
     #your code here
-    new = input.split(' ')
-    print(new)
+    new = s
+    print(s)
+
+    if len(new) > 139 or len(new) == 0:
+        return False
+    else:
+        new = '#' + new
+        return new
 
 
-
+print(generate_hashtag)
 
 
 
