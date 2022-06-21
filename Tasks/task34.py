8fe081c0bbd63fbe77cd358397d183b087ac7d68
Kekin Alexey <agkekin@gmail.com>
2022-06-22 00:39:57 +0300

    task 34  solve

--- Tasks/task34.py
+++ Tasks/task34.py
@@ -1,13 +1,16 @@
+import re
+
+
 def to_underscore(string):
-    for i,j in enumerate(string):
-        # print(i)
-        if i == i.upper():
-            i(j-1) ==
-            print(i)
-    pass
+    ss = str(string)
+
+    result = re.sub(r'([A-Z])', r' \1', ss).split()
+    r = '_'.join(result).lower()
+    return r
+
 
 if __name__ == '__main__':
     assert to_underscore("TestController") == "test_controller"
-    assert to_underscore("MoviesAndBooks") =="movies_and_books"
+    assert to_underscore("MoviesAndBooks") == "movies_and_books"
     assert to_underscore("App7Test") =='app7_test'
-    assert to_underscore(1) =="1"
+    assert to_underscore(1) == "1"
