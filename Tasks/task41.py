f1c1a327d6874250ecdd93993166b67b87b38f8a
alex <alexasdert79>
2022-07-01 21:50:20 +0300

    task 41

--- Tasks/task41.py
+++ Tasks/task41.py
@@ -1,10 +1,29 @@
 def strip_comments(strng, markers):
-    print(strng)
+    # print(strng)
+    ll = ''
+    mlist = []
+    mstr = ''
+    # mstr = strng[strng.find(markers[0]):strng.find(markers[1])]
+    # strng = strng.replace(mstr,'')
+    # print(strng)
+    k = 0
+
     for i in strng:
-        if i == "#":
-            if i == "\"
-                mlist.append(i)
-    pass
+        k += 1
+        if i != markers[0]:
+            mstr += i
+        else:
+            break
+    # print(mstr)
+    print(strng[k::])
+
+    for i in strng[k::]:
+        if i == "\\":
+            mstr += i
+
+    print(mstr)
+
+    return strng
 
 if __name__ == '__main__':
     assert strip_comments('apples, pears # and bananas\ngrapes\nbananas !apples', ['#', '!']) == \
