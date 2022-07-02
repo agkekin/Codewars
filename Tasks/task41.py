90ac7be725dce0d9ea6a7fbeb36cccb46caf46db
Kekin Alexey <agkekin@gmail.com>
2022-07-02 10:15:22 +0300

    task 40 solve

--- Tasks/task41.py
+++ Tasks/task41.py
@@ -1,26 +1,26 @@
-def recurs(a, b):
-    my_str = ''
-    tt = []
-
-    for i in a:
-        for j in i:
-            if j != b:
-                my_str += j
-            else:
-                break
-
-        if len(my_str) > 1:
-            if my_str[-1] == ' ':
-                tt.append(my_str[0:len(my_str)-1])
-            else:
-                tt.append(my_str)
-
-        else:
-            tt.append(my_str)
-
-        my_str = ''
-
-    return tt
+# def recurs(a, b):
+#     my_str = ''
+#     tt = []
+#
+#     for i in a:
+#         for j in i:
+#             if j != b:
+#                 my_str += j
+#             else:
+#                 break
+#
+#         if len(my_str) > 1:
+#             if my_str[-1] == ' ':
+#                 tt.append(my_str[0:len(my_str)-1])
+#             else:
+#                 tt.append(my_str)
+#
+#         else:
+#             tt.append(my_str)
+#
+#         my_str = ''
+#
+#     return tt
 
 def strip_comments(string, markers):
 
@@ -29,19 +29,35 @@ def strip_comments(string, markers):
 
     tt = []
     tt1 = []
-    my_str = ''
+    my_str1 = ''
 
     if not markers:
         return string
 
+    if not string:
+        return string
+
     for x in markers:
-        print(x)
-        print(recurs(recurs(lines, x),x))
+        print('\n')
 
-        ss = '\n'.join(recurs(lines, x))
-    print('ss =', ss)
+        print('x = ', x)
 
-    return ss
+        for i in lines:
+            print('*************************************')
+            print('i = ', i)
+
+            if i.find(x) > 0:
+                print('1')
+                i = i[:i.find(x)]
+            print('my_str = ', i)
+
+            if i[-1] == ' ':
+                i = i[0:len(i) - 1]
+
+            tt.append(i)
+            print('tt =', tt)
+
+    pass
 
 
 if __name__ == '__main__':
