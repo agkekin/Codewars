c1e89d7303d3a25c292a39cd3ff23e837cb2c819
Kekin Alexey <agkekin@gmail.com>
2022-07-05 21:16:56 +0300

    task 43 solve

--- Tasks/task43.py
+++ Tasks/task43.py
@@ -1,25 +1,24 @@
 def sort_array(source_array):
-    print('source_array = ', source_array)
+    # print('source_array = ', source_array)
 
     a = []
-    my_list = source_array
+
+    my_list = source_array.copy()
     my_list_even = my_list.copy()
     my_list_even.sort()
 
-    print('my_list_even.sort = ', my_list_even)
+    # print('my_list_even.sort = ', my_list_even)
 
-    for i in range(len(my_list_even)):
-        print(my_list_even[i])
-        if my_list_even[i] % 2 == 0:
-            my_list_even.remove(i)
-    print('my_list.even = ', my_list_even)
+    my_list_even = [i for i in my_list_even if i % 2]
+
+    # print('my_list.even = ', my_list_even)
 
     for i, j in enumerate(source_array):
         if j % 2 != 0:
             a.append('0')
         else:
             a.append(j)
-    print('a            = ', a)
+    # print('a            = ', a)
 
     ch = 0
 
@@ -27,13 +26,13 @@ def sort_array(source_array):
         if a[i] == '0':
             a[i] = my_list_even[ch]
             ch += 1
-    print(a)
+    # print(a)
     return a
 
 
 if __name__ == '__main__':
-    # assert sort_array([5, 3, 2, 8, 1, 4]) == [1, 3, 2, 8, 5, 4]
-    # assert sort_array([5, 3, 1, 8, 0]) == [1, 3, 5, 8, 0]
-    # assert sort_array([]) == []
+    assert sort_array([5, 3, 2, 8, 1, 4]) == [1, 3, 2, 8, 5, 4]
+    assert sort_array([5, 3, 1, 8, 0]) == [1, 3, 5, 8, 0]
+    assert sort_array([]) == []
     assert sort_array([2, 22, 37, 11, 4, 1, 5, 0]) == [2, 22, 1, 5, 4, 11, 37, 0]
 
---
+++ Tasks/task46.py
@@ -0,0 +1,25 @@
+def warn_the_sheep(queue):
+    num = 0
+
+    for i, j in reversed(list(enumerate(queue))):
+        if j == 'wolf':
+            num = (len(queue) - i - 1)
+    a = ''
+    if num == 0:
+        a = 'Pls go away and stop eating my sheep'
+
+    else:
+        a = f'Oi! Sheep number {num}! You are about to be eaten by a wolf!'
+
+    return a
+
+
+if __name__ == '__main__':
+    # assert warn_the_sheep(['sheep', 'sheep', 'sheep', 'sheep', 'sheep', 'wolf', 'sheep', 'sheep']) ==\
+    #        'Oi! Sheep number 2! You are about to be eaten by a wolf!'
+    # assert warn_the_sheep(['sheep', 'wolf', 'sheep', 'sheep', 'sheep', 'sheep', 'sheep']) == \
+    #        'Oi! Sheep number 5! You are about to be eaten by a wolf!'
+    # assert warn_the_sheep(['wolf', 'sheep', 'sheep', 'sheep', 'sheep', 'sheep', 'sheep']) == \
+    #            'Oi! Sheep number 6! You are about to be eaten by a wolf!'
+    # assert warn_the_sheep(['sheep', 'wolf', 'sheep']) == 'Oi! Sheep number 1! You are about to be eaten by a wolf!'
+    assert warn_the_sheep(['sheep', 'sheep', 'wolf']) == 'Pls go away and stop eating my sheep'
